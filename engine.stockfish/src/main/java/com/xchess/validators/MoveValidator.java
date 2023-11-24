9ccf0a69da2701e4ad11e5addf7162752f4d2276
Guillaume Lerda <guillaume.lerda@busit.com>
2023-11-24 15:57:01 +0100

    fix: added promotion to move syntax validation

--- engine.stockfish/src/main/java/com/xchess/validators/MoveValidator.java
+++ engine.stockfish/src/main/java/com/xchess/validators/MoveValidator.java
@@ -1,9 +1,18 @@
 package com.xchess.validators;
 
+import java.util.Arrays;
+
 public class MoveValidator {
     public static boolean isMoveValid(String move) {
-        return move.length() == 4 &&
-                SquareValidator.isSquareSyntaxValid(move.substring(0, 2)) &&
-                SquareValidator.isSquareSyntaxValid(move.substring(2));
+        if (move.length() == 4) {
+            return SquareValidator.isSquareSyntaxValid(move.substring(0, 2)) &&
+                    SquareValidator.isSquareSyntaxValid(move.substring(2));
+        }
+
+        if (move.length() == 5) {
+            return MoveValidator.isMoveValid(move.substring(0, 4)) && Arrays.asList("q", "r", "b", "n").contains(move.substring(4));
+        }
+
+        return false;
     }
 }
