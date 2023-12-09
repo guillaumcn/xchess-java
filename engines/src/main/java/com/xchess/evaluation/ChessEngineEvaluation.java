5179222bc7945967b63c6218fa6f5cbf7b99cb72
Guillaume Lerda <lerda.guillaume@gmail.com>
2023-12-09 11:54:52 +0100

    test: fix ChessEngineEvaluation equals method

--- engines/src/main/java/com/xchess/evaluation/ChessEngineEvaluation.java
+++ engines/src/main/java/com/xchess/evaluation/ChessEngineEvaluation.java
@@ -4,10 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class ChessEngineEvaluation {
     private ChessEngineEvaluationType type;
     private int value;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessEngineEvaluation that = (ChessEngineEvaluation) o;
+        return value == that.value && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, value);
+    }
 }
