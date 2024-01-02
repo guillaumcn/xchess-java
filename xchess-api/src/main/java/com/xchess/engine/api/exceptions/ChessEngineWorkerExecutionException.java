fb8f7f0368321d1114983cae9df91438e31e59a2
Guillaume Lerda <lerda.guillaume@gmail.com>
2024-01-02 23:05:06 +0100

    chore: moved xchess api to another repo

--- xchess-api/src/main/java/com/xchess/engine/api/exceptions/ChessEngineWorkerExecutionException.java
+++
@@ -1,7 +0,0 @@
-package com.xchess.engine.api.exceptions;
-
-public class ChessEngineWorkerExecutionException extends RuntimeException {
-    public ChessEngineWorkerExecutionException(Throwable cause) {
-        super(cause);
-    }
-}
